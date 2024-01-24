f48c3350191409f4681dded6ab0160ab8db3bd1c
LuanBui02 <luanblp@teamsolutions.vn>
2024-01-24 12:02:56 +0700

    update attribute of orderDto

--- src/main/java/com/finalassignment/assignment/dto/OrderDto.java
+++ src/main/java/com/finalassignment/assignment/dto/OrderDto.java
@@ -1,21 +1,25 @@
 package com.finalassignment.assignment.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.finalassignment.assignment.model.Customer;
+import com.finalassignment.assignment.model.OrderDetail;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDto {
     private int id;
-    private int customerId;
+    private Customer customer;
     @JsonFormat(pattern = "dd/MM/yyyy")
     @DateTimeFormat(pattern = "dd/MM/yyyy")
     private Date orderDate;
     private boolean isComplete;
+    private Set<OrderDetail> orderDetails;
 }
