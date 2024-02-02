57e27fad356bc64e981c7acae7d7cc0384ef1be5
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-02 16:31:12 +0700

    update mapper

--- src/main/java/com/finalassignment/assignment/mapper/CartDetailMapper.java
+++ src/main/java/com/finalassignment/assignment/mapper/CartDetailMapper.java
@@ -6,6 +6,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper
 public interface CartDetailMapper {
     CartDetailMapper INSTANCE = Mappers.getMapper(CartDetailMapper.class);
@@ -13,8 +15,9 @@ public interface CartDetailMapper {
     @Mapping(source = "cart", target = "cartDto")
     @Mapping(source = "item", target = "itemDto")
     CartDetailDto toDto(CartDetail cartDetail);
-
+    List<CartDetailDto> toListDto(List<CartDetail> cartDetails);
     @Mapping(source = "cartDto", target = "cart")
     @Mapping(source = "itemDto", target = "item")
     CartDetail toModel(CartDetailDto cartDetailDto);
+    List<CartDetail> toListModel(List<CartDetailDto> cartDetailsDto);
 }
--- src/main/java/com/finalassignment/assignment/mapper/OrderDetailMapper.java
+++ src/main/java/com/finalassignment/assignment/mapper/OrderDetailMapper.java
@@ -6,6 +6,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper
 public interface OrderDetailMapper {
     OrderDetailMapper INSTANCE = Mappers.getMapper(OrderDetailMapper.class);
@@ -14,7 +16,12 @@ public interface OrderDetailMapper {
     @Mapping(source = "item", target = "itemDto")
     OrderDetailDto toDto(OrderDetail orderDetail);
 
+    List<OrderDetailDto> toListDto(List<OrderDetail> orderDetail);
+
+
     @Mapping(source = "orderDto", target = "order")
     @Mapping(source = "itemDto", target = "item")
     OrderDetail toModel(OrderDetailDto orderDetailDto);
+
+    List<OrderDetail> toListModel(List<OrderDetailDto> orderDetailDto);
 }
