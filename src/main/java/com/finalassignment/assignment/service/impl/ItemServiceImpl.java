31b36123037a1c84439aacae9444e59004b32c0a
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-07 10:28:16 +0700

    update ItemServiceImpl

--- src/main/java/com/finalassignment/assignment/service/impl/ItemServiceImpl.java
+++ src/main/java/com/finalassignment/assignment/service/impl/ItemServiceImpl.java
@@ -11,42 +11,65 @@ import org.mapstruct.factory.Mappers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+
 @Service
+@PropertySource("classpath:message.properties")
 public class ItemServiceImpl implements ItemService {
     @Autowired
     private ItemRepo itemRepo;
     private final ItemMapper itemMapper = Mappers.getMapper(ItemMapper.class);
     private static final Logger logger = LoggerFactory.getLogger(ItemServiceImpl.class);
+    @Value("${Error01}")
+    private String error1;
+    @Value("${Error02}")
+    private String error2;
+    @Value("${Error03}")
+    private String error3;
+    @Value("${ItemFound}")
+    private String itemFound;
+    @Value("${ItemUpdated}")
+    private String itemUpdate;
+    @Value("${Error04}")
+    private String error4;
+    @Value("${ItemDeleted}")
+    private String itemDeleted;
+    @Value("${ItemAdded}")
+    private String itemAdded;
+
     @Override
     public List<ItemDto> showAllItem() {
         List<Item> list = itemRepo.findAll();
-        if(list.isEmpty()) {
-            logger.error("Item Not found");
+        if (list.isEmpty()) {
+            logger.error(error1);
             throw new ItemNotFoundException();
         }
-        logger.info("All items found");
-        return list.stream().map(itemMapper::toDto).sorted().collect(Collectors.toList());
+        logger.info(itemFound);
+        return list.stream().map(itemMapper::toDto).collect(Collectors.toList());
+
     }
 
     @Override
     public Optional<Item> showItemById(int itemId) {
         Optional<Item> itemById = itemRepo.findById(itemId);
         if (itemById.isEmpty()) {
-            logger.error("Not found item by id");
+            logger.error(error2);
             throw new ItemNotFoundByIdException(itemId);
         }
-        logger.info("Item found");
+        logger.info(itemFound);
         return itemById;
     }
 
     @Override
     public void addItems(ItemDto itemDto) {
+        logger.info(itemAdded);
         itemRepo.save(itemMapper.toModel(itemDto));
     }
 
@@ -54,21 +77,21 @@ public class ItemServiceImpl implements ItemService {
     public void updateItem(ItemDto itemDto) {
         Optional<Item> item = itemRepo.findById(itemDto.getId());
         if (item.isEmpty()) {
-            logger.error("Not found item to update");
+            logger.error(error3);
             throw new ItemNotFoundByIdException(itemDto.getId());
         }
-        logger.info("Item updated");
+        logger.info(itemUpdate);
         itemRepo.save(itemMapper.toModel(itemDto));
     }
 
     @Override
     public void deleteItems(int itemId) {
         Optional<Item> list = itemRepo.findById(itemId);
-        if(list.isEmpty()) {
-            logger.error("Not found item to delete");
+        if (list.isEmpty()) {
+            logger.error(error4);
             throw new ItemNotFoundByIdException(itemId);
         }
-        logger.info("Item deleted");
+        logger.info(itemDeleted);
         itemRepo.deleteById(itemId);
     }
 
