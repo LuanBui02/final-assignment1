5bb6cae853bc1b63e7c0b59623077312199d9ce2
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-19 11:38:43 +0700

    update exception

--- src/main/java/com/finalassignment/assignment/service/impl/ItemServiceImpl.java
+++ src/main/java/com/finalassignment/assignment/service/impl/ItemServiceImpl.java
@@ -11,47 +11,37 @@ import org.mapstruct.factory.Mappers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 
 @Service
-@PropertySource("classpath:message.properties")
+@PropertySource("classpath:messages.properties")
 public class ItemServiceImpl implements ItemService {
     @Autowired
     private ItemRepo itemRepo;
     private final ItemMapper itemMapper = Mappers.getMapper(ItemMapper.class);
     private static final Logger logger = LoggerFactory.getLogger(ItemServiceImpl.class);
-    @Value("${Error01}")
-    private String error1;
-    @Value("${Error02}")
-    private String error2;
-    @Value("${Error03}")
-    private String error3;
-    @Value("${ItemFound}")
-    private String itemFound;
-    @Value("${ItemUpdated}")
-    private String itemUpdate;
-    @Value("${Error04}")
-    private String error4;
-    @Value("${ItemDeleted}")
-    private String itemDeleted;
-    @Value("${ItemAdded}")
-    private String itemAdded;
+    @Autowired
+    private MessageSource messageSource;
+
 
     @Override
     public List<ItemDto> showAllItem() {
         List<Item> list = itemRepo.findAll();
         if (list.isEmpty()) {
-            logger.error(error1);
+            String itemNotFound = messageSource.getMessage("ItemNotFound", null, Locale.ENGLISH);
+            logger.error("ItemNotFound: {}", itemNotFound);
             throw new ItemNotFoundException();
         }
-        logger.info(itemFound);
+        String itemFound = messageSource.getMessage("ItemFound", null, Locale.ENGLISH);
+        logger.info("ItemFound: {}", itemFound);
         return list.stream().map(itemMapper::toDto).collect(Collectors.toList());
 
     }
@@ -60,27 +50,32 @@ public class ItemServiceImpl implements ItemService {
     public Optional<Item> showItemById(int itemId) {
         Optional<Item> itemById = itemRepo.findById(itemId);
         if (itemById.isEmpty()) {
-            logger.error(error2);
+            String itemNotFoundById = messageSource.getMessage("NotFoundItemById", null, Locale.ENGLISH);
+            logger.error("ItemNotFoundById: {}", itemNotFoundById);
             throw new ItemNotFoundByIdException(itemId);
         }
-        logger.info(itemFound);
+        String itemFound = messageSource.getMessage("ItemFound", null, Locale.ENGLISH);
+        logger.info("ItemFound: {}", itemFound);
         return itemById;
     }
 
     @Override
     public void addItems(ItemDto itemDto) {
-        logger.info(itemAdded);
+        String itemAdded = messageSource.getMessage("ItemAdded", null, Locale.ENGLISH);
+        logger.info("ItemAdded: {}", itemAdded);
         itemRepo.save(itemMapper.toModel(itemDto));
     }
 
     @Override
     public void updateItem(ItemDto itemDto) {
         Optional<Item> item = itemRepo.findById(itemDto.getId());
+        String itemNotFound = messageSource.getMessage("ItemNotFound", null, Locale.ENGLISH);
         if (item.isEmpty()) {
-            logger.error(error3);
+            logger.error("NotFoundItemToUpdate: {}", itemNotFound);
             throw new ItemNotFoundByIdException(itemDto.getId());
         }
-        logger.info(itemUpdate);
+        String itemUpdate = messageSource.getMessage("ItemUpdated", null, Locale.ENGLISH);
+        logger.info("ItemUpdated: {}", itemUpdate);
         itemRepo.save(itemMapper.toModel(itemDto));
     }
 
@@ -88,10 +83,12 @@ public class ItemServiceImpl implements ItemService {
     public void deleteItems(int itemId) {
         Optional<Item> list = itemRepo.findById(itemId);
         if (list.isEmpty()) {
-            logger.error(error4);
+            String itemNotFound = messageSource.getMessage("ItemNotFound", null, Locale.ENGLISH);
+            logger.error("NotFoundItemToDelete: {}", itemNotFound);
             throw new ItemNotFoundByIdException(itemId);
         }
-        logger.info(itemDeleted);
+        String itemDeleted = messageSource.getMessage("ItemDeleted", null, Locale.ENGLISH);
+        logger.info("ItemDeleted: {}", itemDeleted);
         itemRepo.deleteById(itemId);
     }
 
