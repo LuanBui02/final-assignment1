c336e04307ecf835d797c79467aadf9cdda44080
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-23 13:03:59 +0700

    update OrderController and CartController

--- src/main/java/com/finalassignment/assignment/controller/CartController.java
+++ src/main/java/com/finalassignment/assignment/controller/CartController.java
@@ -1,8 +1,10 @@
 package com.finalassignment.assignment.controller;
 
 import com.finalassignment.assignment.dto.CartDto;
+import com.finalassignment.assignment.dto.CartItemDto;
 import com.finalassignment.assignment.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,23 +24,29 @@ public class CartController {
     private CartService cartService;
 
     @GetMapping("/{customerId}")
-    public List<CartDto> getCartByCustomer(@PathVariable int customerId) {
-        return cartService.showCart(customerId);
+    public CartDto getCartByCustomer(@PathVariable int customerId) {
+        return cartService.showCartDto(customerId);
     }
 
     @PostMapping()
-    public void addNewItemToCart(@RequestBody CartDto cartDto) {
-        cartService.addItemToCart(cartDto);
+    public CartDto addNewItemToCart(@RequestBody CartItemDto cartItemDto) {
+        return cartService.addItemToCart(cartItemDto);
     }
 
     @PutMapping()
-    public void updateNewItemInCart() {
-        cartService.updateItemInCart();
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateNewItemInCart(@RequestBody CartItemDto cartItemDto) {
+        cartService.updateItemInCart(cartItemDto);
     }
 
     @DeleteMapping("/{cartDetailId}")
     public void deleteItemFromCart(@PathVariable int cartDetailId) {
         cartService.deleteItemFromCart(cartDetailId);
+    }
+
+    @GetMapping
+    public List<CartDto> showCart() {
+        return cartService.showAllCart();
     }
 }
 
--- src/main/java/com/finalassignment/assignment/controller/OrderController.java
+++ src/main/java/com/finalassignment/assignment/controller/OrderController.java
@@ -1,14 +1,18 @@
 package com.finalassignment.assignment.controller;
 
+import com.finalassignment.assignment.dto.OrderCustomerDto;
 import com.finalassignment.assignment.dto.OrderDto;
 import com.finalassignment.assignment.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -17,17 +21,22 @@ public class OrderController {
     private OrderService orderService;
 
     @GetMapping("/{customerId}")
-    public OrderDto getOrderOfCustomer(@PathVariable int customerId) {
-        return orderService.showOrder(customerId);
+    public List<OrderDto> getOrderOfCustomer(@PathVariable int customerId) {
+        return orderService.showOrderDto(customerId);
     }
 
     @PostMapping()
-    public void createOrder(@RequestBody OrderDto orderDto) {
-        orderService.addOrder(orderDto);
+    public void createOrder(@RequestBody OrderCustomerDto orderCustomerDto) {
+        orderService.addOrder(orderCustomerDto);
     }
 
     @GetMapping("/latestOrder/{customerId}")
     public OrderDto getOrderLatest(@PathVariable int customerId) {
         return orderService.showOrderLatest(customerId);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public void deleteOrder(@PathVariable int orderId) {
+        orderService.deleteOrder(orderId);
     }
 }
