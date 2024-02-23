affc01adf24aac97e22ff73b23b68b365a4d9970
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-23 13:05:44 +0700

    format Application

--- src/test/java/com/finalassignment/assignment/AssignmentApplicationTests.java
+++ src/test/java/com/finalassignment/assignment/AssignmentApplicationTests.java
@@ -1,13 +1,27 @@
 package com.finalassignment.assignment;
 
+import com.finalassignment.assignment.dto.CartDto;
+import com.finalassignment.assignment.mapper.CartMapper;
+import com.finalassignment.assignment.mapper.CustomerMapper;
+import com.finalassignment.assignment.model.Cart;
 import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class AssignmentApplicationTests {
+    private final CartMapper cartMapper = Mappers.getMapper(CartMapper.class);
+    private final CustomerMapper customerMapper = Mappers.getMapper(CustomerMapper.class);
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void ModelToDto() {
+        CartDto cart = new CartDto();
+        cart.setId(1);
+
+        Cart model = cartMapper.toModel(cart);
+        assertEquals(cart.getId(), model.getId());
+    }
 
 }
