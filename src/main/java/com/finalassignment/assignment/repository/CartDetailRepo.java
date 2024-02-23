e3eea0bc8cd683a82e43710329da5051d44c76e4
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-23 13:14:54 +0700

    update CartRepo

--- src/main/java/com/finalassignment/assignment/repository/CartDetailRepo.java
+++ src/main/java/com/finalassignment/assignment/repository/CartDetailRepo.java
@@ -4,7 +4,15 @@ import com.finalassignment.assignment.model.CartDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CartDetailRepo extends JpaRepository<CartDetail, Integer> {
+    List<CartDetail> findCartDetailsByCartId(int cartId);
+
+    List<CartDetail> findListCartDetailByCartId(int cartId);
+
+    Optional<CartDetail> findCartDetailByCartId(int cartId);
 }
 
--- src/main/java/com/finalassignment/assignment/repository/CartRepo.java
+++ src/main/java/com/finalassignment/assignment/repository/CartRepo.java
@@ -4,7 +4,9 @@ import com.finalassignment.assignment.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepo extends JpaRepository<Cart, Integer> {
-
+    public Optional<Cart> findCartByCustomerId(int customerId);
 }
