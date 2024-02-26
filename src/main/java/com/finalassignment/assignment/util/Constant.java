837a502aaaa90ce283d7792ae37f759ea91c16bf
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-26 12:29:04 +0700

    add Constant class

---
+++ src/main/java/com/finalassignment/assignment/util/Constant.java
@@ -0,0 +1,19 @@
+package com.finalassignment.assignment.util;
+
+public class Constant {
+
+    public static final String itemNotFoundById = "Item not found by Id";
+    public static final String itemFoundById = "Item found by Id";
+    public static final String itemNotFound = "Item not found";
+    public static String finalItemFound = "All items found";
+    public static final String itemUpdated = "Item updated";
+    public static final String itemDeleted = "Item deleted";
+    public static final String itemAdded = "Item added";
+    public static final String customerNotFound = "Customer not found";
+    public static String finalCartFound = "Cart found";
+    public static String finalCartNotFoundById = "Not found cart by id";
+    public static final String cartAdded = "Cart added";
+    public static final String orderFound = "All order for customer";
+    public static final String orderAdded = "Order added";
+    public static final String cartDetailNotFound = "Not found id in CartDetail";
+}
