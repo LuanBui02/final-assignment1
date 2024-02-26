928231ed57a41ef21231637556df20b3c6b29e0f
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-26 13:29:29 +0700

    update Constant text

--- src/main/java/com/finalassignment/assignment/util/Constant.java
+++ src/main/java/com/finalassignment/assignment/util/Constant.java
@@ -5,13 +5,13 @@ public class Constant {
     public static final String itemNotFoundById = "Item not found by Id";
     public static final String itemFoundById = "Item found by Id";
     public static final String itemNotFound = "Item not found";
-    public static String finalItemFound = "All items found";
+    public static final String itemFound = "All items found";
     public static final String itemUpdated = "Item updated";
     public static final String itemDeleted = "Item deleted";
     public static final String itemAdded = "Item added";
     public static final String customerNotFound = "Customer not found";
-    public static String finalCartFound = "Cart found";
-    public static String finalCartNotFoundById = "Not found cart by id";
+    public static final  String cartFound = "Cart found";
+    public static final String cardNotFoundById = "Not found cart by id";
     public static final String cartAdded = "Cart added";
     public static final String orderFound = "All order for customer";
     public static final String orderAdded = "Order added";
