2ecedb8a9005c5a57e1b0e0bf3862a47e4eddf02
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-27 13:35:33 +0700

    update AbstractMessage class

--- src/main/java/com/finalassignment/assignment/util/AbstractMessage.java
+++ src/main/java/com/finalassignment/assignment/util/AbstractMessage.java
@@ -12,56 +12,7 @@ import java.util.Locale;
 public class AbstractMessage {
     @Autowired
     private MessageSource messageSource;
-
-    public String getItemFound(String itemFound) {
-        return messageSource.getMessage(itemFound, null, Locale.ENGLISH);
-    }
-
-    public String getItemNotFound(String itemNotFound) {
-        return messageSource.getMessage(itemNotFound, null, Locale.ENGLISH);
-    }
-
-    public String getItemNotFoundById(String itemNotFoundById) {
-        return messageSource.getMessage(itemNotFoundById, null, Locale.ENGLISH);
-    }
-
-    public String getItemUpdated(String itemUpdated) {
-        return messageSource.getMessage(itemUpdated, null, Locale.ENGLISH);
-    }
-
-    public String getItemDeleted(String itemDeleted) {
-        return messageSource.getMessage(itemDeleted, null, Locale.ENGLISH);
-    }
-
-    public String getItemAdded(String itemAdded) {
-        return messageSource.getMessage(itemAdded, null, Locale.ENGLISH);
-    }
-
-    public String getCustomerNotFound(String customerNotFound) {
-        return messageSource.getMessage(customerNotFound, null, Locale.ENGLISH);
-    }
-
-    public String getCartFound(String cartFound) {
-        return messageSource.getMessage(cartFound, null, Locale.ENGLISH);
-    }
-
-    public String getCartNotFoundById(String cartNotFoundById) {
-        return messageSource.getMessage(cartNotFoundById, null, Locale.ENGLISH);
-    }
-
-    public String getCartAdded(String cartAdded) {
-        return messageSource.getMessage(cartAdded, null, Locale.ENGLISH);
-    }
-
-    public String getOrderFound(String orderFound) {
-        return messageSource.getMessage(orderFound, null, Locale.ENGLISH);
-    }
-
-    public String getOrderAdded(String orderAdded) {
-        return messageSource.getMessage(orderAdded, null, Locale.ENGLISH);
-    }
-
-    public String getCartDetailNotFound(String cartDetailNotFound) {
-        return messageSource.getMessage(cartDetailNotFound, null, Locale.ENGLISH);
+    public String getMessage(String key) {
+        return messageSource.getMessage(key, null, Locale.ENGLISH);
     }
 }
