d83c4c64c894f9afff0bb1d935912924738b1f15
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-27 13:38:17 +0700

    remove  getAllCart

--- src/main/java/com/finalassignment/assignment/service/CartService.java
+++ src/main/java/com/finalassignment/assignment/service/CartService.java
@@ -3,8 +3,6 @@ package com.finalassignment.assignment.service;
 import com.finalassignment.assignment.dto.CartDto;
 import com.finalassignment.assignment.dto.CartItemDto;
 
-import java.util.List;
-
 public interface CartService {
     CartDto showCartDto(int customerId);
 
@@ -13,6 +11,4 @@ public interface CartService {
     void updateItemInCart(CartItemDto cartItemDto);
 
     void deleteItemFromCart(int cartDetailId);
-
-    List<CartDto> showAllCart();
 }
