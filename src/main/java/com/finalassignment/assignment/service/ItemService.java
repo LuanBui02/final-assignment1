076e59c6d0e825023bac1cc47fab9c4c1cea50b9
LuanBui02 <luanblp@teamsolutions.vn>
2024-02-28 12:13:20 +0700

    update ItemService

--- src/main/java/com/finalassignment/assignment/service/ItemService.java
+++ src/main/java/com/finalassignment/assignment/service/ItemService.java
@@ -1,15 +1,13 @@
 package com.finalassignment.assignment.service;
 
 import com.finalassignment.assignment.dto.ItemDto;
-import com.finalassignment.assignment.model.Item;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemService {
     List<ItemDto> showAllItem();
 
-    Optional<Item> showItemById(int itemId);
+    ItemDto showItemById(int itemId);
 
     void addItems(ItemDto itemDto);
 
