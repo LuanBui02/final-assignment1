5768131b9b552cd37274bd285c8ded6d194945d2
LuanBui02 <luanblp@teamsolutions.vn>
2024-03-01 15:12:36 +0700

    Update OrderService

--- src/main/java/com/finalassignment/assignment/service/OrderService.java
+++ src/main/java/com/finalassignment/assignment/service/OrderService.java
@@ -1,6 +1,5 @@
 package com.finalassignment.assignment.service;
 
-import com.finalassignment.assignment.dto.OrderCustomerDto;
 import com.finalassignment.assignment.dto.OrderDto;
 
 import java.util.List;
@@ -8,7 +7,7 @@ import java.util.List;
 public interface OrderService {
     List<OrderDto> showOrderDto(int customerId);
 
-    void addOrder(OrderCustomerDto orderCustomerDto);
+    void addOrder(OrderDto orderDto) ;
 
     OrderDto showOrderLatest(int customerId);
 }
