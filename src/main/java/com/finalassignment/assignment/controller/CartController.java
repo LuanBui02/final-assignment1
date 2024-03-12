862cfb32ec6fbae3485d132f64bfb3e8b06c383e
LuanBui02 <luanblp@teamsolutions.vn>
2024-03-12 09:00:43 +0700

    add CrossOrigin

--- src/main/java/com/finalassignment/assignment/controller/CartController.java
+++ src/main/java/com/finalassignment/assignment/controller/CartController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/carts")
 public class CartController extends  AbstractMessage{
     @Autowired
